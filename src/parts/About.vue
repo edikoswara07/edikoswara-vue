d5643e48cc7ce631f2bbb8f510a6d5315583677e
Edi Koswara <edikoswara1st@gmail.com>
2021-05-19 01:48:04 +0700

    custom section about

--- src/parts/About.vue
+++ src/parts/About.vue
@@ -1,6 +1,8 @@
 <template>
+<div class="py-12 bg-blueGray-100">
+
 <Container>
-	<div class="flex flex-col-reverse items-center justify-between my-16 md:flex-row space-x-0 md:space-x-8 space-y-8 md:space-y-0">
+	<div class="flex flex-col-reverse items-start justify-between md:flex-row space-x-0 md:space-x-8 space-y-8 md:space-y-0">
 	<div class="w-full md:w-6/12">
 		<div>
 			<img :src="datas.image" alt="aboutImage" class="inset-0 object-cover object-center w-full h-96 rounded-xl">
@@ -8,10 +10,12 @@
 	</div>
 	<div class="w-full md:w-6/12">
 		<h3 class="text-2xl font-semibold lg:text-4xl text-blueGray-900">{{datas.title}}</h3>
-		<p class="my-4 text-base font-medium leading-relaxed text-blueGray-700">{{datas.description}}</p>
+		<p class="my-4 text-base font-medium leading-relaxed md:my-4 text-blueGray-700">{{datas.description}}</p>
+		<p class="my-4 text-base font-medium leading-relaxed md:my-4 text-blueGray-700">{{datas.description}}</p>
 	</div>
 	</div>
 </Container>
+</div>
 </template>
 
 <script>
