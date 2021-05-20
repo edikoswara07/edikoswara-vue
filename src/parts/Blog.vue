c84aaa918ae2e261040bce07fdb0d95b84b5cf3c
Edi Koswara <edikoswara1st@gmail.com>
2021-05-20 23:57:59 +0700

    menambahkan blog section

---
+++ src/parts/Blog.vue
@@ -0,0 +1,90 @@
+<template>
+	<div class="py-12 my-12">
+		<Container>
+			<div class="flex flex-col items-center justify-center space-y-4">
+				<h3
+					class="text-2xl font-semibold md:text-4xl text-blueGray-900"
+				>
+					From Blog
+				</h3>
+				<p
+					class="text-base font-medium text-center md:text-lg text-blueGray-700"
+				>
+					membaca adalah seni untuk membuat anda pusing akan pikiran
+					saya meheheheh
+				</p>
+			</div>
+
+			<div class="grid grid-cols-1 md:grid-cols-3 gap-4 py-6 mt-12">
+				<div
+					class="w-full p-4 bg-blueGray-100 rounded-lg shadow-md"
+					v-for="data in blogDatas"
+					:key="data.id"
+				>
+					<div class="w-full h-48 md:h-72 relative">
+						<div
+							class="absolute top-2 right-2 py-2 text-sm px-6 font-semibold bg-blueGray-900 text-blueGray-100 rounded-lg"
+						>
+							{{ data.tag }}
+						</div>
+						<img
+							:src="data.image"
+							alt="blogImage"
+							class="object-cover object-center h-full w-full inset-0 rounded-lg"
+						/>
+					</div>
+					<h3
+						class="text-base md:text-lg font-semibold text-blueGray-900 py-6"
+					>
+						{{ data.title }}
+					</h3>
+					<div>
+						<p
+							class="text-sm text-blueGray-700 font-medium leading-relaxed"
+						>
+							{{ data.content.substring(0, 150) }}...
+						</p>
+					</div>
+					<div class="flex justify-between items-center mt-10">
+						<div class="inline-flex items-center">
+							<div class="relative w-8 h-8">
+								<img
+									:src="data.imageAuthor"
+									alt="blog"
+									class="inset-0 object-cover object-center w-full h-full rounded-full"
+								/>
+							</div>
+							<h3
+								class="ml-2 text-blueGray-700 font-medium text-sm"
+							>
+								{{ data.author }}
+							</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Container>
+	</div>
+</template>
+
+<script>
+import Container from "../components/Container.vue";
+export default {
+	name: "Blog",
+	props: ["blogDatas"],
+	data() {
+		return {
+			image:
+				"https://images.unsplash.com/photo-1525382455947-f319bc05fb35?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGFuZGF8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+			title: "Panda is awesome",
+			text:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam enim vel eum modi atque temporibus alias, natus officia ratione delectus quam recusandae id. Ex facilis repellat ipsa? Rerum, autem aliquid.",
+		};
+	},
+	components: {
+		Container,
+	},
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,35 @@
 	<Navbar />
 	<Hero></Hero>
 	<About :datas="datas"></About>
+	<Blog :blogDatas="blogDatas"></Blog>
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
 import Hero from "../parts/Hero.vue";
 import About from "../parts/About.vue";
+import Blog from "../parts/Blog.vue";
+import blogJson from "../data/Blog.json";
+
 export default {
 	name: "Home",
 	data() {
-	return {
-		datas: 
-			{
+		return {
+			datas: {
 				title: "Tentang Edi Koswara",
-				image: "https://images.unsplash.com/photo-1540126034813-121bf29033d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80",
-				description: "I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.",
-			}
-		}
+				image:
+					"https://images.unsplash.com/photo-1540126034813-121bf29033d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80",
+				description:
+					"I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.",
+			},
+			blogDatas: blogJson,
+		};
 	},
 	components: {
 		Navbar,
 		Hero,
-		About
+		About,
+		Blog,
 	},
 };
 </script>
